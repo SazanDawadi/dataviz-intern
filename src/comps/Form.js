64cffb6b68c0a333d8f4b99ca263bf75e56abe41
SazanDawadi <sajandawadi5@gmail.com>
2022-10-14 19:15:42 +0545

    add form submission

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import formFeild from '../schema/formFields.json';
 import '../styles/form.css'
 
 const Form = () => {
+    const [state, setState] = useState({});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(state);
+      }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setState({ ...state, [name]: value });
+      };
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>{formFeild.form.form_name}</h1>
             {formFeild.form.feilds.map(inputData => {
                 return(
                     <div className='input-container'>
                         <label>{inputData.label}</label>
-                        <input type= {inputData.html_element} placeholder = {inputData.name} />
+                        <input name = {inputData.name} type= {inputData.html_element} placeholder = {inputData.name} onChange={handleChange}  />
                         <br/>
                     </div>
 
